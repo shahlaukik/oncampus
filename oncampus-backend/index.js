daeb2099e1878060f1dc243e9a1b309930d43219
Laukik Shah <78136178+shahlaukik@users.noreply.github.com>
2022-08-10 17:42:56 +0530

    Added cors

--- oncampus-backend/index.js
+++ oncampus-backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const favicon = require("serve-favicon");
 const path = require("path");
+var cors = require('cors')
 require("dotenv").config();
 
 // Importing utility
@@ -38,6 +39,8 @@ const app = express();
 // Configuring Express to use static files
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+
+app.use(cors());
 
 // Encoding url
 app.use(express.urlencoded({ extended: true }));
